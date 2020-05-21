70681c77b14354609ae692b5b5654586d0230a58
George Travis <wewerelikesharks@gmail.com>
2020-05-20 20:43:30 -0700

    transferred new user logic from user route to user controller

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,30 +6,34 @@ const User = require(`../../database/models/User`);
 const passport = require(`../../passport`);
 const mongoose = require(`mongoose`);
 
-router.post(`/`, (req, res) => {
-  console.log(`user signup`);
+const userController = require(`../../controllers/usersController`);
 
-  const { username, password } = req.body;
-  // ADD VALIDATION
-  User.findOne({ username: username }, (err, user) => {
-    if (err) {
-      console.log(`User.js post error: `, err);
-    } else if (user) {
-      res.json({
-        error: `Sorry, already a user with the username: ${username}`,
-      });
-    } else {
-      const newUser = new User({
-        username: username,
-        password: password,
-      });
-      newUser.save((err, savedUser) => {
-        if (err) return res.json(err);
-        res.json(savedUser);
-      });
-    }
-  });
-});
+router.route(`/`).post(userController.newUser);
+
+// router.post(`/`, (req, res) => {
+//   console.log(`user signup`);
+
+//   const { username, password } = req.body;
+//   // ADD VALIDATION
+//   User.findOne({ username: username }, (err, user) => {
+//     if (err) {
+//       console.log(`User.js post error: `, err);
+//     } else if (user) {
+//       res.json({
+//         error: `Sorry, already a user with the username: ${username}`,
+//       });
+//     } else {
+//       const newUser = new User({
+//         username: username,
+//         password: password,
+//       });
+//       newUser.save((err, savedUser) => {
+//         if (err) return res.json(err);
+//         res.json(savedUser);
+//       });
+//     }
+//   });
+// });
 
 router.post(
   `/login`,
