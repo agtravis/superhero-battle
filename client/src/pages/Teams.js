d77dab56e66b0e3ec6be20e1642048c15a33f446
George Travis <wewerelikesharks@gmail.com>
2020-07-22 14:52:52 -0700

    team slots all work correctly

--- client/src/pages/Teams.js
+++ client/src/pages/Teams.js
@@ -14,8 +14,6 @@ class Teams extends Component {
       slot1: null,
       slot2: null,
       slot3: null,
-      contender1: null,
-      teams: null,
     };
   }
 
@@ -35,8 +33,18 @@ class Teams extends Component {
     API.removeFromTeam(this.props.currentUser._id, event.target.dataset.id)
       .then(() => {
         this.props.fillUser();
+        this.setState({ slot1: null, slot2: null, slot3: null });
       })
       .catch(err => console.error(err));
+  };
+
+  inTeam = character => {
+    for (const member of this.props.teams) {
+      if (member._id === character._id) {
+        return false;
+      }
+    }
+    return true;
   };
 
   render() {
@@ -78,8 +86,78 @@ class Teams extends Component {
                 </div>
               )}
             </PreFightDivWrapper>
-            <PreFightDivWrapper>2</PreFightDivWrapper>
-            <PreFightDivWrapper>3</PreFightDivWrapper>
+            <PreFightDivWrapper teams={this.props.teams}>
+              {this.props.teams[1] ? (
+                <p
+                  data-id={this.props.teams[1]._id}
+                  onClick={event => this.removeTeamMember(event)}
+                >
+                  {this.props.teams[1].name}
+                </p>
+              ) : (
+                <div>
+                  {this.state.slot2 === null ? (
+                    <button
+                      onClick={() => this.setState({ slot2: `choosing` })}
+                    >
+                      Get your second team member!
+                    </button>
+                  ) : (
+                    <div>
+                      <p>Team Member #2</p>
+                      <div>
+                        {this.props.roster
+                          .filter(character => this.inTeam(character))
+                          .map((character, index) => (
+                            <RosterSelectionSlot
+                              key={index}
+                              index={index}
+                              character={character}
+                              getContender={this.getContender}
+                            />
+                          ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              )}
+            </PreFightDivWrapper>
+            <PreFightDivWrapper teams={this.props.teams}>
+              {this.props.teams[2] ? (
+                <p
+                  data-id={this.props.teams[2]._id}
+                  onClick={event => this.removeTeamMember(event)}
+                >
+                  {this.props.teams[2].name}
+                </p>
+              ) : (
+                <div>
+                  {this.state.slot3 === null ? (
+                    <button
+                      onClick={() => this.setState({ slot3: `choosing` })}
+                    >
+                      Get your third team member!
+                    </button>
+                  ) : (
+                    <div>
+                      <p>Team Member #3</p>
+                      <div>
+                        {this.props.roster
+                          .filter(character => this.inTeam(character))
+                          .map((character, index) => (
+                            <RosterSelectionSlot
+                              key={index}
+                              index={index}
+                              character={character}
+                              getContender={this.getContender}
+                            />
+                          ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              )}
+            </PreFightDivWrapper>
           </div>
         ) : (
           <div>
