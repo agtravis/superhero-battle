d6d7cc25a7d911ebafb65e8255fbae51826e0548
George Travis <wewerelikesharks@gmail.com>
2020-11-18 14:23:36 -0800

    added color palette

---
+++ client/src/config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: `#e2b340`,
+  secondary: `#5b5194`,
+  black: `#222`,
+  white: `#f2f2f2`,
+  mediumGrey: `#6e6969`,
+  light: `#c4c4c4`,
+  danger: `#aa1515`,
+  dark: `#333`,
+};
