6a45702700162117208e95fcf36dcf6f91f444cc
George Travis <wewerelikesharks@gmail.com>
2020-12-20 12:04:40 -0800

    roster - added loading animation

--- client/src/pages/Roster.js
+++ client/src/pages/Roster.js
@@ -6,10 +6,14 @@ import API from "../utils/API";
 import IndexPortrait from "../components/IndexPortrait";
 import AppButton from "../components/AppButton";
 
+import { TailSpin } from "@agney/react-loading";
+import colors from "../config/colors";
+
 class Roster extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      rosterLoaded: false,
       roster: [],
       newCharacter: null,
       newCharacterLoaded: false,
@@ -19,6 +23,14 @@ class Roster extends Component {
       cheatTeamIds2: null,
       cheatTeamIds3: null,
     };
+  }
+
+  componentDidMount() {
+    API.getUserDetails(this.props.currentUser._id)
+      .then(response =>
+        this.setState({ rosterLoaded: true, roster: response.data.roster })
+      )
+      .catch(err => console.error(err));
   }
 
   noImage = () => {
@@ -77,16 +89,6 @@ class Roster extends Component {
       .catch(err => console.error(err));
   };
 
-  removeFromRoster = characterId => {
-    API.removeCharacterFromRoster(this.props.currentUser._id, {
-      characterId: characterId,
-    })
-      .then(() => {
-        this.props.fillUser();
-      })
-      .catch(err => console.error(err));
-  };
-
   prestige = () => {
     API.prestige(this.props.currentUser._id)
       .then(() => {
@@ -103,93 +105,100 @@ class Roster extends Component {
     return (
       <div>
         <h1>Your Roster</h1>
-        <p>
-          ({this.props.roster.length}
-          /731 recruited ||{` `}
-          {((this.props.roster.length / 731) * 100).toFixed(2)}% complete)
-        </p>
-        {this.props.roster.length >= 731 ? (
-          <div>
-            <h1>You have filled up your roster!</h1>
-            <p>
-              <button onClick={() => this.prestige()}>Click</button> to
-              Prestige!
-            </p>
-            <p>This will empty your roster but level up your Prestige!</p>
+
+        {!this.state.rosterLoaded ? (
+          <div
+            style={{
+              display: `flex`,
+              justifyContent: `center`,
+            }}
+          >
+            <TailSpin
+              className={`loading-animation`}
+              width={`100`}
+              style={{
+                backgroundColor: colors.lightPrimary,
+              }}
+            />
           </div>
         ) : (
           <div>
-            {/*DELETE CHEAT BUTTON */}
-            {this.props.roster.length < 1 ? (
+            <p>
+              ({this.state.roster.length}
+              /731 recruited ||{` `}
+              {((this.state.roster.length / 731) * 100).toFixed(2)}% complete)
+            </p>
+            {this.state.roster.length >= 731 ? (
               <div>
-                {!this.state.newCharacterLoaded ? (
-                  <div>
-                    <p>You do not have anyone in your roster yet!</p>
-                    <button onClick={() => this.getFirstTeamMember()}>
-                      Click to get your first team member!
-                    </button>{" "}
-                    <p>Get full team</p>
-                    {this.props.roster.length < 3 ? (
-                      <button onClick={() => this.getTeam()}>Cheat!</button>
-                    ) : null}
-                  </div>
-                ) : (
-                  <div
-                    style={{
-                      display: `flex`,
-                      justifyContent: `center`,
-                      flexDirection: `column`,
-                    }}
-                  >
-                    <div>
-                      <IndexPortrait
-                        round
-                        showStats
-                        character={this.state.newCharacter}
-                      />
-                    </div>
-                    <div
-                      style={{
-                        display: `flex`,
-                        justifyContent: `center`,
-                      }}
-                    >
-                      <AppButton
-                        width={`200px`}
-                        margin={`10px  0px`}
-                        onClick={this.addToRoster}
-                      >
-                        Add To Roster!
-                      </AppButton>
-                    </div>
-                  </div>
-
-                  // <div>
-                  //   <p>{this.state.newCharacter.name}</p>
-                  //   {this.state.newCharacter.image.url &&
-                  //   this.state.imageValid ? (
-                  //     <img
-                  //       src={this.state.newCharacter.image.url}
-                  //       alt={this.state.newCharacter.name}
-                  //       onError={() => this.noImage()}
-                  //     />
-                  //   ) : (
-                  //     <p>No Image on File!</p>
-                  //   )}
-                  //   <button onClick={() => this.addToRoster()}>
-                  //     Add to Roster!
-                  //   </button>
-                  // </div>
-                )}
+                <h1>You have filled up your roster!</h1>
+                <p>
+                  <button onClick={() => this.prestige()}>Click</button> to
+                  Prestige!
+                </p>
+                <p>This will empty your roster but level up your Prestige!</p>
               </div>
             ) : (
-              this.props.roster.map((character, index) => (
-                <div key={index}>
-                  <h2 style={{ textAlign: `center` }}>{index + 1}</h2>
-                  <IndexPortrait character={character} showStats size={200} />
-                  {index < this.props.roster.length - 1 && <hr />}
-                </div>
-              ))
+              <div>
+                {/*DELETE CHEAT BUTTON */}
+                {this.state.roster.length < 1 ? (
+                  <div>
+                    {!this.state.newCharacterLoaded ? (
+                      <div>
+                        <p>You do not have anyone in your roster yet!</p>
+                        <button onClick={() => this.getFirstTeamMember()}>
+                          Click to get your first team member!
+                        </button>{" "}
+                        <p>Get full team</p>
+                        {this.state.roster.length < 3 ? (
+                          <button onClick={() => this.getTeam()}>Cheat!</button>
+                        ) : null}
+                      </div>
+                    ) : (
+                      <div
+                        style={{
+                          display: `flex`,
+                          justifyContent: `center`,
+                          flexDirection: `column`,
+                        }}
+                      >
+                        <div>
+                          <IndexPortrait
+                            round
+                            showStats
+                            character={this.state.newCharacter}
+                          />
+                        </div>
+                        <div
+                          style={{
+                            display: `flex`,
+                            justifyContent: `center`,
+                          }}
+                        >
+                          <AppButton
+                            width={`200px`}
+                            margin={`10px  0px`}
+                            onClick={this.addToRoster}
+                          >
+                            Add To Roster!
+                          </AppButton>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  this.state.roster.map((character, index) => (
+                    <div key={index}>
+                      <h2 style={{ textAlign: `center` }}>{index + 1}</h2>
+                      <IndexPortrait
+                        character={character}
+                        showStats
+                        size={200}
+                      />
+                      {index < this.state.roster.length - 1 && <hr />}
+                    </div>
+                  ))
+                )}
+              </div>
             )}
           </div>
         )}
