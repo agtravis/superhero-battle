1fc691c9dfeefac989702e00c0d0d7baa3dc91d7
George Travis <wewerelikesharks@gmail.com>
2020-12-31 09:14:52 -0800

    small screen leader board works

--- client/src/components/LeaderBoardFlow/FullScreen/LeaderBoardHeaderRow/index.js
+++ client/src/components/LeaderBoardFlow/FullScreen/LeaderBoardHeaderRow/index.js
@@ -2,18 +2,36 @@ import React, { Component } from "react";
 import LeaderBoardHeaderCell from "../../LeaderBoardHeaderCell";
 
 class LeaderBoardHeaderRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerCells: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ headerCells: this.props.headerCells });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.headerCells !== prevProps.headerCells) {
+      this.setState({ headerCells: this.props.headerCells });
+    }
+  }
+
   render() {
     return (
       <tr>
-        {this.props.headerCells.map((current, index) => (
-          <LeaderBoardHeaderCell
-            key={index}
-            cellStyle={this.props.cellStyle}
-            onClick={current.onClick}
-            title={current.title}
-            params={current.params}
-          />
-        ))}
+        {this.state.headerCells &&
+          this.state.headerCells.map((current, index) => (
+            <LeaderBoardHeaderCell
+              key={index}
+              cellStyle={this.props.cellStyle}
+              onClick={current.onClick}
+              title={current.title}
+              params={current.params}
+            />
+          ))}
       </tr>
     );
   }
--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Breakpoint } from "react-socks";
 import LeaderBoardTable from "../components/LeaderBoardFlow/FullScreen/LeaderBoardTable";
-import LoadingAnimation from "../components/LoadingAnimation";
 import PageTitle from "../components/PageTitle";
 import API from "../utils/API";
 import colors from "../config/colors";
+import LeaderBoardTableMobile from "../components/LeaderBoardFlow/Mobile/LeaderBoardTableMobile";
 
 class Leaderboard extends Component {
   constructor(props) {
@@ -12,6 +12,8 @@ class Leaderboard extends Component {
     this.state = {
       topTen: null,
       usersLoaded: false,
+      metric: `prestige`,
+      headerColumns: [],
     };
   }
 
@@ -22,6 +24,54 @@ class Leaderboard extends Component {
       textAlign: `center`,
       width: `25px`,
     },
+  };
+
+  changeMetric = value => {
+    let thirdColumnIndex;
+    switch (value) {
+      case `ranking`:
+        thirdColumnIndex = 6;
+        this.getTopScorers();
+        break;
+      case `username`:
+        thirdColumnIndex = 6;
+        this.findAll(`username`);
+        break;
+      case `battles`:
+        thirdColumnIndex = 2;
+        this.sortByPropertyName(`fights`, `desc`);
+        break;
+      case `wins`:
+        thirdColumnIndex = 3;
+        this.sortByPropertyName(`wins`, `desc`);
+        break;
+      case `losses`:
+        thirdColumnIndex = 4;
+        this.sortByPropertyName(`losses`, `desc`);
+        break;
+      case `winPercentage`:
+        thirdColumnIndex = 5;
+        this.findAll(`percentage`);
+        break;
+      case `prestige`:
+        thirdColumnIndex = 6;
+        this.sortByPropertyName(`prestige`, `desc`);
+        break;
+      case `rosterLength`:
+        thirdColumnIndex = 7;
+        this.findAll(`roster`);
+        break;
+      default:
+        thirdColumnIndex = 6;
+        this.getTopScorers();
+    }
+    const headerColumns = this.state.headerColumns;
+    headerColumns.pop();
+    headerColumns.push(this.headerCells[thirdColumnIndex]);
+    this.setState({
+      headerColumns: headerColumns,
+      metric: value,
+    });
   };
 
   convertDate = datestamp => {
@@ -42,6 +92,12 @@ class Leaderboard extends Component {
 
   componentDidMount() {
     this.getTopScorers();
+    const headerColumns = [
+      this.headerCells[0],
+      this.headerCells[1],
+      this.headerCells[6],
+    ];
+    this.setState({ headerColumns: headerColumns });
   }
 
   getTopScorers = () => {
@@ -110,32 +166,85 @@ class Leaderboard extends Component {
     this.setState({ topTen: sorted, usersLoaded: true });
   };
 
+  headerCells = [
+    {
+      onClick: this.getTopScorers,
+      title: `Ranking`,
+      params: [],
+    },
+    {
+      onClick: this.findAll,
+      title: `Username`,
+      params: [`username`],
+    },
+    {
+      onClick: this.sortByPropertyName,
+      title: `Battles`,
+      params: [`fights`, `desc`],
+    },
+    {
+      onClick: this.sortByPropertyName,
+      title: `Wins`,
+      params: [`wins`, `desc`],
+    },
+    {
+      onClick: this.sortByPropertyName,
+      title: `Losses`,
+      params: [`losses`, `desc`],
+    },
+    {
+      onClick: this.findAll,
+      title: `Win Percentage`,
+      params: [`percentage`],
+    },
+    {
+      onClick: this.sortByPropertyName,
+      title: `Prestige Level`,
+      params: [`prestige`, `desc`],
+    },
+    {
+      onClick: this.findAll,
+      title: `Currently in Roster`,
+      params: [`roster`],
+    },
+  ];
+
   render() {
     return (
       <div>
         <PageTitle>Leader Board</PageTitle>
-        {!this.state.usersLoaded ? (
-          <LoadingAnimation divHeight={400} size={150} />
-        ) : (
-          <div
-            style={{ width: `100%`, display: `flex`, justifyContent: `center` }}
-          >
-            <Breakpoint medium up>
-              <LeaderBoardTable
-                cellStyle={this.styles.cellStyle}
-                getTopScorers={this.getTopScorers}
-                findAll={this.findAll}
-                sortByPropertyName={this.sortByPropertyName}
-                topTen={this.state.topTen}
-                convertDate={this.convertDate}
-                convertWinPercentage={this.convertWinPercentage}
-              />
-            </Breakpoint>
-            <Breakpoint small down>
-              <p>small table</p>
-            </Breakpoint>
-          </div>
-        )}
+        <p style={{ textAlign: `center` }}>
+          <em>- Sort by any metric -</em>
+        </p>
+        <div
+          style={{ width: `100%`, display: `flex`, justifyContent: `center` }}
+        >
+          <Breakpoint medium up>
+            <LeaderBoardTable
+              usersLoaded={this.state.usersLoaded}
+              headerCells={this.headerCells}
+              cellStyle={this.styles.cellStyle}
+              topTen={this.state.topTen}
+              convertDate={this.convertDate}
+              convertWinPercentage={this.convertWinPercentage}
+            />
+          </Breakpoint>
+          <Breakpoint small down>
+            <LeaderBoardTableMobile
+              usersLoaded={this.state.usersLoaded}
+              changeMetric={this.changeMetric}
+              metric={this.state.metric}
+              headerCells={this.state.headerColumns}
+              cellStyle={this.styles.cellStyle}
+              getTopScorers={this.getTopScorers}
+              findAll={this.findAll}
+              sortByPropertyName={this.sortByPropertyName}
+              topTen={this.state.topTen}
+              convertDate={this.convertDate}
+              convertWinPercentage={this.convertWinPercentage}
+            />
+          </Breakpoint>
+        </div>
       </div>
     );
   }
