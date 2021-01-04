7b773cd0a81af6434bf9fa2c66b70fac22c51ddc
George Travis <wewerelikesharks@gmail.com>
2021-01-04 11:50:30 -0800

    searching for users and heros looks good - needs functionality

---
+++ client/src/components/SearchResult/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import SuperHeroAPI from "../../utils/SuperHeroAPI";
+import LoadingAnimation from "../LoadingAnimation";
+
+class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageLoaded: false,
+      imageUrl: ``,
+      imageValid: true,
+      noCharacter: false,
+    };
+  }
+
+  componentDidMount() {
+    this.loadImage();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.name !== prevProps.name) {
+      this.loadImage();
+    }
+  }
+
+  loadImage = () => {
+    if (this.props.type === `user` && this.props.image) {
+      SuperHeroAPI.loadContender(this.props.image)
+        .then(response => {
+          if (response.data.image.url) {
+            this.setState({
+              imageUrl: response.data.image.url,
+              imageLoaded: true,
+            });
+          }
+        })
+        .catch(err => console.error(err));
+    } else if (!this.props.image) {
+      this.setState({ noCharacter: true, imageLoaded: true });
+    } else if (this.props.type === `hero` && this.props.image) {
+      this.setState({
+        imageUrl: this.props.image,
+        imageLoaded: true,
+      });
+    }
+  };
+
+  noImage = () => {
+    this.setState({ imageValid: false });
+  };
+
+  styles = {
+    container: { display: `flex`, height: `75px` },
+    image: {
+      borderRadius: `30px`,
+      height: `60px`,
+      objectFit: `scale-down`,
+      objectPosition: `center top`,
+    },
+    imageContainer: {
+      alignItems: `center`,
+      display: `flex`,
+      flex: `1`,
+      justifyContent: `center`,
+    },
+    infoContainer: {
+      alignItems: `center`,
+      display: `flex`,
+      flex: `3`,
+      justifyContent: `center`,
+      textAlign: `center`,
+    },
+    noImageContainer: {
+      alignItems: `center`,
+      display: `flex`,
+      textAlign: `center`,
+    },
+    text: { margin: `0px` },
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.index !== 0 && <hr />}
+        <div style={this.styles.container}>
+          <div style={this.styles.imageContainer}>
+            {!this.state.imageLoaded && (
+              <LoadingAnimation divHeight={60} size={50} />
+            )}
+            {this.state.imageLoaded && this.state.imageValid && (
+              <img
+                src={this.state.imageUrl}
+                alt={this.props.name}
+                onError={() => this.noImage()}
+                style={this.styles.image}
+              />
+            )}
+            {(this.state.noCharacter || !this.state.imageValid) && (
+              <div style={this.styles.noImageContainer}>
+                <p style={this.styles.text}>N/A</p>
+              </div>
+            )}
+          </div>
+          <div style={this.styles.infoContainer}>
+            <p style={this.styles.text}>
+              {this.props.name}
+              {this.props.type === `user` &&
+                ` - prestige: ${this.props.prestige}, recruited: ${this.props.rosterLength}, since ${this.props.registered}`}
+              {this.props.type === `hero` &&
+                (this.props.info === `-`
+                  ? ` - no known affiliations.`
+                  : ` - affiliations: ${this.props.info}.`)}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchResult;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import SearchForm from "../components/SearchForm";
-import ToggleSwitch from "../components/ToggleSwitch";
 import API from "../utils/API";
 import SuperHeroAPI from "../utils/SuperHeroAPI";
+import SearchForm from "../components/SearchForm";
+import SearchResult from "../components/SearchResult";
+import ToggleSwitch from "../components/ToggleSwitch";
 
 class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isUserSearch: true,
-      searchField: ``,
       searched: false,
+      searchField: ``,
       results: [],
     };
   }
@@ -18,6 +19,11 @@ class SearchPage extends Component {
   clearForm = () => {
     document.getElementById(`user-search`).value = ``;
     this.setState({ searchField: `` });
+  };
+
+  convertDate = date => {
+    const dateArr = new Date(date).toDateString().split(` `);
+    return `${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`;
   };
 
   handleChange = (event, stateKey) => {
@@ -53,9 +59,9 @@ class SearchPage extends Component {
     return (
       <div>
         <ToggleSwitch
+          height={`100px`}
           leftText={`USER`}
           rightText={`HERO`}
-          height={`100px`}
           toggleFunction={this.toggleSearchMode}
         />
         <SearchForm
@@ -67,11 +73,27 @@ class SearchPage extends Component {
         />
         {this.state.searched === `user` &&
           this.state.results.map((user, index) => (
-            <p key={index}>{user.username}</p>
+            <SearchResult
+              image={user.roster[0]}
+              index={index}
+              key={index}
+              name={user.username}
+              prestige={user.prestige + 1}
+              registered={this.convertDate(user.registered)}
+              rosterLength={user.roster.length}
+              type={`user`}
+            />
           ))}
         {this.state.searched === `hero` &&
           this.state.results.map((hero, index) => (
-            <p key={index}>{hero.name}</p>
+            <SearchResult
+              image={hero.image.url}
+              index={index}
+              info={hero.connections[`group-affiliation`]}
+              key={index}
+              name={hero.name}
+              type={`hero`}
+            />
           ))}
       </div>
     );
