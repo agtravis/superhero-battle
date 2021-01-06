71c54c512068fe9e9f078cc3e97e5a77436a608a
George Travis <wewerelikesharks@gmail.com>
2021-01-06 14:37:03 -0800

    Image and stats component

---
+++ client/src/components/CharacterFlow/ImageAndStats/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import IndexPortraitStats from "../../IndexPortraitStats";
+
+class ImageAndStats extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageValid: true,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.name !== prevProps.name) {
+      this.setState({ imageValid: true });
+    }
+  }
+
+  styles = {
+    container: {
+      alignItems: `center`,
+      display: `flex`,
+      flexWrap: `wrap`,
+      justifyContent: `space-evenly`,
+      margin: `50px 0px`,
+    },
+    image: { borderRadius: `10px`, width: `100%` },
+    imageContainer: { width: `350px` },
+    statsContainer: {
+      alignItems: `center`,
+      display: `flex`,
+      height: ` 350px`,
+      justifyContent: `center`,
+      padding: `20px`,
+      width: `350px`,
+    },
+  };
+
+  noImage = () => {
+    this.setState({ imageValid: false });
+  };
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        <div style={this.styles.imageContainer}>
+          {this.state.imageValid ? (
+            <img
+              style={this.styles.image}
+              src={this.props.character.image.url}
+              alt={this.props.character.name}
+              onError={() => this.noImage()}
+            />
+          ) : (
+            <p>No image!</p>
+          )}
+        </div>
+        <div style={this.styles.statsContainer}>
+          <IndexPortraitStats character={this.props.character} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageAndStats;
--- client/src/pages/Character.js
+++ client/src/pages/Character.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import ImageAndStats from "../components/CharacterFlow/ImageAndStats";
+import PageTitle from "../components/PageTitle";
 
 class Character extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.character !== prevProps.character) {
+      this.setState({ imageValid: true });
+    }
+  }
+
   render() {
     if (!this.props.currentUser) {
       window.location.href = `/`;
     }
     const character = this.props.location.state.character;
-    return <div>{character.name}</div>;
+    return (
+      <div>
+        <div>
+          <PageTitle>{character.name}</PageTitle>
+        </div>
+        <ImageAndStats character={character} />
+      </div>
+    );
   }
 }
 
