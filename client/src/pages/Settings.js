c2d006b7f27d702bedef3c38942f5470cc13df00
George Travis <wewerelikesharks@gmail.com>
2021-01-07 16:00:38 -0800

    ready to be broken to components on password changed

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AppButton from "../components/AppButton";
+import AppInput from "../components/AppInput";
 import PageTitle from "../components/PageTitle";
 import API from "../utils/API";
 import bcrypt from "bcryptjs";
@@ -8,46 +9,76 @@ class Settings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      old: ``,
       new: ``,
+      newAgain: ``,
+      old: ``,
+      showOldInput: false,
+      showNewInput: false,
+      showStartButton: true,
+      passwordChanged: false,
+      error: false,
+      errorMessage: null,
     };
   }
 
-  componentDidMount() {
-    API.getUserDetails(this.props.currentUser._id)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(err => console.error(err));
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    if (this.state.new.length < 1) {
-      console.log(`new password not long enough`);
+  setError = message => {
+    if (message) {
+      this.setState({ error: true, errorMessage: message });
     } else {
-      API.getUserDetails(this.props.currentUser._id)
-        .then(response => {
-          bcrypt.compare(this.state.old, response.data.password, (err, res) => {
-            if (err) {
-              console.error(err);
-            } else if (res) {
-              API.changePassword({
-                id: this.props.currentUser._id,
-                newPassword: this.state.new,
-              })
-                .then(response => console.log(response.data))
-                .catch(err => console.error(err));
-            } else {
-              console.log(`password does not match`);
-            }
-          });
-        })
-        .catch(err => console.error(err));
+      this.setState({ error: false, errorMessage: null });
     }
   };
 
-  handleChange = (value, type) => this.setState({ [type]: value });
+  verifyNewPassword = event => {
+    event.preventDefault();
+    this.setError();
+    if (this.state.new.length < 4) {
+      this.setError(`Password must be at least 4 characters in length!`);
+    } else {
+      if (this.state.new !== this.state.newAgain) {
+        this.setError(`the second password does not match the first`);
+      } else if (this.state.new === this.state.newAgain) {
+        this.setError();
+        API.changePassword({
+          id: this.props.currentUser._id,
+          newPassword: this.state.new,
+        })
+          .then(() => {
+            this.toggle([`showNewInput`, `showStartButton`, `passwordChanged`]);
+          })
+          .catch(err => console.error(err));
+      }
+    }
+  };
+
+  verifyOldPassword = event => {
+    event.preventDefault();
+    if (this.state.old.length < 1) {
+      this.setError(`Please enter a password`);
+    } else {
+      this.setError();
+      API.getUserDetails(this.props.currentUser._id).then(response => {
+        bcrypt.compare(this.state.old, response.data.password, (err, res) => {
+          if (err) {
+            console.error(err);
+          } else if (res === false) {
+            this.setError(`Password is incorrect`);
+          } else if (res === true) {
+            this.toggle([`showOldInput`, `showNewInput`]);
+          }
+        });
+      });
+    }
+  };
+
+  handleChange = (event, field) =>
+    this.setState({ [field]: event.target.value });
+
+  toggle = states => {
+    for (const state of states) {
+      this.setState({ [state]: !this.state[state] });
+    }
+  };
 
   render() {
     if (!this.props.currentUser) {
@@ -56,17 +87,57 @@ class Settings extends Component {
     return (
       <div>
         <PageTitle>Settings</PageTitle>
-        <form onSubmit={event => this.handleSubmit(event)}>
-          <input
-            value={this.state.old}
-            onChange={event => this.handleChange(event.target.value, `old`)}
-          />
-          <input
-            value={this.state.new}
-            onChange={event => this.handleChange(event.target.value, `new`)}
-          />
-          <AppButton type={`submit`}>CLICK</AppButton>
-        </form>
+        {this.state.showStartButton && (
+          <div>
+            <AppButton
+              onClick={() => this.toggle([`showOldInput`, `showStartButton`])}
+            >
+              Change Password
+            </AppButton>
+          </div>
+        )}
+        {this.state.showOldInput && (
+          <div>
+            <form onSubmit={event => this.verifyOldPassword(event)}>
+              <p>Enter your old password</p>
+              <AppInput
+                value={this.state.old}
+                handleChange={this.handleChange}
+                fieldName={`old`}
+              />
+              <AppButton type={`submit`}>Continue</AppButton>
+            </form>
+          </div>
+        )}
+        {this.state.showNewInput && (
+          <div>
+            <form onSubmit={event => this.verifyNewPassword(event)}>
+              <p>Enter your new password</p>
+              <AppInput
+                value={this.state.new}
+                handleChange={this.handleChange}
+                fieldName={`new`}
+              />
+              <p>Enter your new password again</p>
+              <AppInput
+                value={this.state.newAgain}
+                handleChange={this.handleChange}
+                fieldName={`newAgain`}
+              />
+              <AppButton type={`submit`}>Change Password</AppButton>
+            </form>
+          </div>
+        )}
+        {this.state.error && (
+          <div>
+            <p>{this.state.errorMessage}</p>
+          </div>
+        )}
+        {this.state.passwordChanged && (
+          <div>
+            <p>Password Changed</p>
+          </div>
+        )}
       </div>
     );
   }
