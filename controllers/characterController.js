6ea68fb3d31223283c175b2f644af6ee0fc5b0f0
George Travis <wewerelikesharks@gmail.com>
2021-01-26 09:55:05 -0800

    characterController.js

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,33 +1,10 @@
 "use strict";
 
 const Character = require(`../database/models/Character`);
-const mongoose = require(`mongoose`);
 
 const errorResponseCode = 422;
 
 module.exports = {
-  randomCharacter: (req, res) => {
-    const query = { state: `OK` };
-    const totalCharacters = 731;
-    const randomNum = Math.floor(Math.random() * totalCharacters);
-    Character.find()
-      .limit(1)
-      .skip(randomNum)
-      .then(dbCharacter => res.json(dbCharacter))
-      .catch(err => console.error(err));
-  },
-  searchByInheritedId: (req, res) => {
-    Character.find({
-      id: req.body.id,
-    })
-      .then(dbCharacters => res.json(dbCharacters))
-      .catch(err => res.json(err));
-  },
-  getCharacter: (req, res) => {
-    Character.findById(req.params.id)
-      .then(dbCharacter => res.json(dbCharacter))
-      .catch(err => res.json(err));
-  },
   findCharacterByName: (req, res) => {
     Character.find({
       name: {
@@ -36,6 +13,27 @@ module.exports = {
       },
     })
       .then(dbCharacter => res.json(dbCharacter))
-      .catch(err => res.json(err));
+      .catch(err => res.status(errorResponseCode).json(err));
+  },
+  getCharacter: (req, res) => {
+    Character.findById(req.params.id)
+      .then(dbCharacter => res.json(dbCharacter))
+      .catch(err => res.status(errorResponseCode).json(err));
+  },
+  randomCharacter: (req, res) => {
+    const totalCharacters = 731;
+    const randomNum = Math.floor(Math.random() * totalCharacters);
+    Character.find()
+      .limit(1)
+      .skip(randomNum)
+      .then(dbCharacter => res.json(dbCharacter))
+      .catch(err => res.status(errorResponseCode).json(err));
+  },
+  searchByInheritedId: (req, res) => {
+    Character.find({
+      id: req.body.id,
+    })
+      .then(dbCharacters => res.json(dbCharacters))
+      .catch(err => res.status(errorResponseCode).json(err));
   },
 };
